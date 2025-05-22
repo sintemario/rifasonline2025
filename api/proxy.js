ceb54701eebb3f87a222c12cdc50cb362b26a5f5
mario <99936124+sintemario@users.noreply.github.com>
2025-05-21 22:07:22 -0300

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,1 +1,10 @@
-
+export default async (req, res) => {
+  const API_URL = "https://script.google.com/macros/s/AKfycbxWj7-50CBqvEM-eT9dwSmQ5HbR7mLdMp6YW6Q5F3ge8izCYBQhv3zQcQ4q99SZW2AQ5Q/exec";
+  const response = await fetch(API_URL, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: req.body
+  });
+  const data = await response.json();
+  res.status(200).json(data);
+};
